faa78e861761e6efc6cbd9264b9c582ad7bfced8
KJ <ksheerjas@gmail.com>
2026-04-09 14:08:42 +1000

    Update fig2_bulk_plots.r

--- fig2/fig2_bulk_plots.r
+++ fig2/fig2_bulk_plots.r
@@ -1,3 +1,16 @@
+# mapping clusters:
+
+cluster_map <- data.frame(
+  seurat_clusters = c("0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"),
+  celltype = c("primitive-RBC","Mk-ery_prog","Mk",
+               "early_lymphoid_progenitor","HSC",
+               "basophil-mast_progenitor","pre-cDC","Ery",
+               "Mk","CDP","CD14+Monocyte","CMP-GMP",
+               "Ery","Ery","Mk","unknown")
+)
+
+df <- left_join(df, cluster_map, by = "seurat_clusters")
+
 library(Seurat)
 library(dplyr)
 library(ggplot2)
